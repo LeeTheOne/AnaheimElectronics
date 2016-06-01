806c3e198c03561118c22e3621f8447a1aaa1bda
zacharyli <truefreedom@qq.com>
2016-06-01 16:20:55 +0800

    add recycler view

---
+++ app/src/main/java/com/anaheimelectronics/developing/NormalRecyclerViewAdapter.java
@@ -0,0 +1,48 @@
+package com.anaheimelectronics.developing;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.anaheimelectronics.R;
+
+/**
+ * Created by zacharyMac on 16/6/1.
+ */
+public class NormalRecyclerViewAdapter extends RecyclerView.Adapter<NormalRecyclerViewAdapter.NormalTextViewHolder>{
+    private final LayoutInflater mLayoutInflater;
+    private final Context mContext;
+    private String[] mTitles;
+
+    public NormalRecyclerViewAdapter(Context context) {
+        mTitles = context.getResources().getStringArray(R.array.titles);
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public NormalRecyclerViewAdapter.NormalTextViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new NormalTextViewHolder(mLayoutInflater.inflate(R.layout.developing_item_view,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(NormalRecyclerViewAdapter.NormalTextViewHolder holder, int position) {
+        holder.mTextView.setText(mTitles[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTitles == null ? 0 : mTitles.length;
+    }
+
+    public static class NormalTextViewHolder extends RecyclerView.ViewHolder{
+        TextView mTextView;
+        public NormalTextViewHolder(View itemView) {
+            super(itemView);
+            mTextView = (TextView)itemView.findViewById(R.id.main_text);
+        }
+    }
+}
--- app/src/main/java/com/anaheimelectronics/launch/AEMainActivity.java
+++ app/src/main/java/com/anaheimelectronics/launch/AEMainActivity.java
@@ -1,17 +1,20 @@
 package com.anaheimelectronics.launch;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.anaheimelectronics.R;
 import com.anaheimelectronics.common.AERootActivity;
+import com.anaheimelectronics.developing.NormalRecyclerViewAdapter;
 
 public class AEMainActivity extends AERootActivity {
 
+    static final String TAG = "AEMainActivity";
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void initData(Context context, AttributeSet attrs) {
@@ -29,11 +32,14 @@ public class AEMainActivity extends AERootActivity {
 
     @Override
     protected void bindView() {
-
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
     }
 
     @Override
     protected void initView() {
-
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));//这里用线性显示 类似于listview
+//        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));//这里用线性宫格显示 类似于grid view
+//        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, OrientationHelper.VERTICAL));//这里用线性宫格显示 类似于瀑布流
+        mRecyclerView.setAdapter(new NormalRecyclerViewAdapter(this));
     }
 }
