979f38cd5d84b5976c3209e35e713fe0b73598b9
zacharyli <304652843@qq.com>
2016-12-03 16:04:10 +0800

    add a activity

---
+++ app/src/main/java/com/anaheimelectronics/controller/CustomAlbumActivity.java
@@ -0,0 +1,10 @@
+package com.anaheimelectronics.controller;
+
+import com.anaheimelectronics.common.AERootActivity;
+
+/**
+ * Created by Administrator on 2016/12/3.
+ */
+
+public class CustomAlbumActivity extends AERootActivity {
+}
